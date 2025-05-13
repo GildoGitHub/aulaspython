b3ffa521be594275ce59bed83a1f7341cc5aefa0
unknown <GildoGitHub>
2025-05-13 16:03:30 +0200

    aula 1

---
+++ AULA1/main.py
@@ -0,0 +1,11 @@
+# ESSA INSTRUÇÃO PERMITE IMPRIMIR INFORMAÇÃO
+print("MEU PRIMEIRO PROGRAMA PYTHON")
+
+#print() é uma função em python que permite receber uma informação e imprimir na tela
+# A informação "MEU PRIMEIRO PROGRAMA PYTHON" será exibida na tela
+# Essa informação pode ser diferente, sinta-se avondade para escrever qualquer coisa
+
+# ESTE JOGO DA VELHA PARA PORTUGUES BRASILEIRO OU CARDINAL PARA PORTUGUES MOÇAMBICANO
+# É USADO PARA INSERIR COMENTÁRIOS NO CÓDIGO EM PYTHON.
+
+#COMENTÁRIOS AJUNDAM A EXPLICAR O CÓDIGO.
