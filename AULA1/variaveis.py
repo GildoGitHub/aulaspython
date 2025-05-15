9820d5e65c0617fab39804d90304e504b2a91484
unknown <GildoGitHub>
2025-05-15 14:20:36 +0200

    aula 2

---
+++ AULA1/variaveis.py
@@ -0,0 +1,19 @@
+#Tipos de variaves
+#TIPO STRING - Usado para armazenar palavras
+# As variaves STRING ficam entre aspas simples (' ') ou aspas duplas (" ").
+
+#Declaração de variaveis do tipo STRING
+nome_planeta = "Marte" #A variável (nome_planeta) armazena um nome de planeta
+nome_pais = "Wacanda" #A variável (nome_pais) armazena um nome de um país
+
+#Declaração de variaveis do tipo inteiro
+idade = 13 #A variável (idade) armazena idade 13 anos
+numero_filhos = 14 #A variável (numero_filhos) armazena numero de filhos, 14 filhos
+
+#Declaração de variaveis do tipo float (numeros com parte decimal)
+altura = 1.6 #A variável (altura) armazena altura de 1.6 metros
+media_trimestral = 12.5 #A variável (media_trimestral) armazena media trimestral de um aluno.
+
+#Imprimindo informações das variaveis
+print(idade) #Imprime idade (13)
+print(nome_planeta) #Imprime nome do planeta (Marte)
